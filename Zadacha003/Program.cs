331c7e1de2f5bb8cbc551ae01cdf3042a25be1a5
Artur <kourovartur@yandex.ru>
2022-08-21 23:39:28 +0300

    chetnoe

---
+++ Zadacha003/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает число и выдаёт, является ли число чётным?
+int a;
+string input_a;
+
+Console.WriteLine("Введите число");
+input_a = Console.ReadLine();
+
+a = Int32.Parse(input_a);
+
+if (a % 2 > 0)
+{
+    Console.WriteLine("не четное: " +a);
+}
+else 
+{
+    Console.WriteLine("четное: " +a);
+}
+

5cf210d9743625b19b1a1305b499e33e00991a76
Artur <kourovartur@yandex.ru>
2022-08-22 12:41:23 +0300

    от_1_до_N

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+
+int N;
+
+string input_N;
+
+Console.WriteLine("Введите число");
+input_N = Console.ReadLine();
+N = Int32.Parse(input_N);
+
+int count = 1;
+    
+while (count < N ) // включительно("N-1" , будет выдовать до числа N)
+{
+     if (count % 2 > 0)
+     {
+         count++;
+     }
+     {
+     Console.WriteLine(count + " ");
+     count++;
+     }
+}

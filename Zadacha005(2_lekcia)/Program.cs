f6d337b33da0f5b621ee5305e4ef9a3e1e3c783d
Artur <kourovartur@yandex.ru>
2022-08-25 20:16:43 +0300

    naiti_2_chislo

---
+++ Zadacha005(2_lekcia)/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает на вход случайно сгенерированное трёхзначное число и на выходе показывает вторую цифру этого числа.
+//1 диапазон 
+int start = 100;
+int stop = 1000;
+
+// 2 генерируем числа
+int value = new Random().Next(start,stop);
+Console.WriteLine(value);
+
+// 3 извлечение 2й цифры
+int digit_first = value / 10;
+int digit_second = digit_first % 10;
+
+// Выводим результат
+Console.WriteLine("второе число " +digit_second);

ba7cbd79b05ebefc6607eff4cd968d47f48edc66
Artur <kourovartur@yandex.ru>
2022-08-26 10:04:09 +0300

    решено_не_до_конца

---
+++ Zadacha006(2_lekcia)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.
+//1 диапазон 
+int start = 1;
+int stop = 65536;
+
+// 2 генерируем числа
+//int value = new Random().Next(start,stop);
+int value = 123;
+Console.WriteLine(value);
+
+// 3 извлечение 2й цифры
+int digit_numb1 = value / 100;
+int digit_numb2 = digit_numb1 % 10;
+int digit_numb3 = digit_numb2 % 1000;
+
+// Выводим результат
+Console.WriteLine("Третье число " +digit_numb3);
+
+if (value < 99)
+{
+    Console.WriteLine("Третьего числа нет");
+}

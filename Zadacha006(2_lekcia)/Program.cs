6354e352d1a27f9493a8f4afac4030bd0a335f42
Artur <kourovartur@yandex.ru>
2022-08-29 17:36:49 +0300

    таблица _кубов

--- Zadacha006(2_lekcia)/Program.cs
+++ Zadacha006(2_lekcia)/Program.cs
@@ -1,19 +1,20 @@
 ﻿// Задача 2: Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.
-//1 диапазон 
+//1. диапазон 
 int start = 1;
 int stop = 65536;
 
-// 2 генерируем числа
-int value = new Random().Next(start,stop);
-//int value = 99;
+// 2. генерируем числа
+//int value = new Random().Next(start,stop);
+int value = 7234568;
 Console.WriteLine(value);
 
-// 3 извлечение 3й цифры   считает справа на лево
-int digit_numb1 = value % 10;
-int digit_numb2 = (value % 100) / 10;
+// 3. извлечение 3й цифры   считает справа на лево
+//int digit_numb1 = value % 10;
+//int digit_numb2 = (value % 100) / 10;
+
 int digit_numb3 = (value % 1000) / 100;
 
-// Выводим результат
+// 4. Выводим результат
 
 
 if (value <= 99)
